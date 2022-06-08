e383ddfaff7383410075ddda5fd08a54b5af0ac0
Euber Santos <euber.olavo.bezerra@gmail.com>
2022-06-08 00:02:22 -0300

    Final Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from 'react';
+import fetchData from './services/api';
+import initialData from './helpers/initialData';
+
+import Card from './components/Card';
 
 function App() {
-  return <h1>Olá Mundo</h1>;
+  const [city, setCity] = useState('');
+  const [data, setData] = useState(initialData);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchData(city).then((response) => {
+      setData(response);
+    });
+  };
+
+  return (
+    <div className="flex flex-col w-full h-screen items-center justify-center">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Cidade"
+          className="p-3 rounded-lg outline-none"
+          value={city}
+          onChange={({ target: { value } }) => setCity(value)}
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold"
+        >
+          Pesquisar
+        </button>
+      </form>
+
+      <Card data={data} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+const KEY = '84c19401da40419db7802605220806';
+
+const fetchData = async (city) => {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+
+  const fetchResponse = await fetch(url);
+  const data = await fetchResponse.json();
+
+  return data;
+};
+
+export default fetchData;
